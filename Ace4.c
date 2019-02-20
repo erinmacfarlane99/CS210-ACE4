d642b6f8774a98a2a2bae1c7a863384c6ad9acf0
Tom Latham <tomlatham@Toms-MacBook-Air-2.local>
2019-02-20 22:11:24 +0000

    1 and 2 fully working

--- Ace4.c
+++ Ace4.c
@@ -4,72 +4,110 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <errno.h>
 
-void display_prompt();
-void parse(char input[512]);
-void exCommand();
+#define MAX_ARGS 50
+
+// char** parse(char input[512], char cmd[30], char *tokenisedArray[MAX_ARGS]);
+// void exCommand(char cmd[30], char *tokenisedArray[MAX_ARGS]);
 
 
 int main() {
+  char *tokenisedArray[MAX_ARGS];
+  char input[512];
+  char cmd[30];
+  char *delimiters = " \t\n|<>&;";
 
-	display_prompt();
-	exCommand();
+  while (1) {
 
-	return 0;
-}
+        printf(">");
+        fgets(input,512,stdin);
+        // tokenises input
+        int i=0;
+        char* token;
 
-//Display Prompt function
-void display_prompt() {
-	
-	char input[512];
-	
+        token = strtok(input, delimiters);
+        tokenisedArray[0] = token;
+        while (token != NULL) {
+          i++;
+          token = strtok(NULL, delimiters);
+          tokenisedArray[i] = token;
 
-	printf(">");
-	fgets(input,512,stdin);
-	
-	printf("%s", input);	
-	parse(input);
-	
-	//if (*input=="exit") {
-	//	printf("Identical");
-	//}
-	//else {
-	//	printf("Not identical");
-	//}
-	
-}
+        }
+        // end of tokenising
+        if ((strcmp("exit", tokenisedArray[0]) == 0) || getchar()==EOF) {
+            break;
+        }
+        // executes command
+        pid_t pid = fork();
+
+        //fork returns a negative value: error has occured
+        if (pid == -1) {
+          perror("Error\n");
+          exit(1);
+        }
+
+        //fork returns 0: the child process is running
+        else if (pid == 0) {
+               if(execvp(tokenisedArray[0],tokenisedArray) == -1 ){
+                     printf("%s: Command not found\n",tokenisedArray[0]);
+
+               }
+
+        }
+        //parent process
+        else {
+          int status;
+          waitpid(pid, &status, 0);
+          printf("Child Complete\n");
+        }
+      //  exCommand(cmd, tokenisedArray);
+    }
+
+    return 0;
+  }
 
 //Parsing input function
-void parse(char input[512]) {
+// char** parse(char input[512], char cmd[30], char *tokenisedArray[MAX_ARGS]) {
 
-	char* token;
-	char* token2;
-	token = strtok(input, " |&<>;");
+//   int i=0;
+// 	char* token;
+// 	token = strtok(input, " |&<>;");
+//   strcpy(cmd, token);
+//   printf("%s\n", cmd);
+// 	while (input != NULL && i<50) {
+// 		token = strtok(NULL, " |&<>;");
+//     tokenisedArray[i] = token;
+//     i++;
+// 	}
+//   return tokenisedArray;
+// }
 
-	while (token != NULL) {
-		printf("%s\n", token);
-		token = strtok(NULL, " |&<>;");
-	}
-}
-
-void exCommand() {
-
-	pid_t pid = fork();
-	int status;
-
-	//fork returns a negative value: error has occured
-	if (pid < 0) { 
-		perror("Error");
-		exit(1);
-	}
-	//fork returns 0: the child process is running
-	else if (pid == 0) { 
-		execvp(*argv, argv);
-	}
-	//parent process
-	else { 
-		wait(&status);
-		printf("Child Complete");
-	}
-
-}
+// void exCommand(char cmd[30], char *tokenisedArray[MAX_ARGS]) {
+//
+// 	pid_t pid = fork();
+//
+//   printf("%s\n", cmd);
+// 	//fork returns a negative value: error has occured
+// 	if (pid == -1) {
+// 		perror("Error\n");
+//     exit(1);
+// 	}
+//
+// 	//fork returns 0: the child process is running
+// 	else if (pid == 0) {
+//          if(execvp(tokenisedArray[0],tokenisedArray) == -1 ){
+//                printf("%s: Command not found\n",tokenisedArray[0]);
+//
+//          }
+//
+// 	}
+// 	//parent process
+// 	else {
+//     int status;
+// 		waitpid(pid, &status, 0);
+// 		printf("Child Complete\n");
+// 	}
+//
+// }
