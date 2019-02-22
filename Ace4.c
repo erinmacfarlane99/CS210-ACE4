77294c3a6b67d1fced41a2a04df769e183d3ec0e
Tom Latham <tomlatham@Toms-MacBook-Air-2.local>
2019-02-22 14:54:29 +0000

    part 3 finished successfully.

--- Ace4.c
+++ Ace4.c
@@ -10,14 +10,27 @@
 #define MAX_ARGS 50
 
 // char** parse(char input[512], char cmd[30], char *tokenisedArray[MAX_ARGS]);
-// void exCommand(char cmd[30], char *tokenisedArray[MAX_ARGS]);
+void get_path(char *tokenisedArray[MAX_ARGS]);
+void get_path(char *tokenisedArray[MAX_ARGS]);
+void exCommand(char *tokenisedArray[MAX_ARGS]);
+
 
 
 int main() {
   char *tokenisedArray[MAX_ARGS];
   char input[512];
-  char cmd[30];
   char *delimiters = " \t\n|<>&;";
+  char *curPath = getenv("PATH");
+  printf("%s\n", curPath);
+  char *getHome = getenv("HOME");
+
+  if (chdir(getHome)==0){
+    printf("Path set to Home.\n");
+  }
+  else{
+    printf("Error setting home directory\n");
+  }
+
 
   while (1) {
 
@@ -36,34 +49,20 @@ int main() {
 
         }
         // end of tokenising
-        if ((strcmp("exit", tokenisedArray[0]) == 0) || getchar()==EOF) {
+        if ((strcmp("exit", tokenisedArray[0]) == 0)) {
             break;
         }
+        else if (getchar()==EOF){
+          break;
+        }
         // executes command
-        pid_t pid = fork();
 
-        //fork returns a negative value: error has occured
-        if (pid == -1) {
-          perror("Error\n");
-          exit(1);
-        }
-
-        //fork returns 0: the child process is running
-        else if (pid == 0) {
-               if(execvp(tokenisedArray[0],tokenisedArray) == -1 ){
-                     printf("%s: Command not found\n",tokenisedArray[0]);
-
-               }
-
-        }
-        //parent process
-        else {
-          int status;
-          waitpid(pid, &status, 0);
-          printf("Child Complete\n");
-        }
-      //  exCommand(cmd, tokenisedArray);
+        exCommand(tokenisedArray);
     }
+    printf("Shutting down shell...\n");
+    setenv("PATH", curPath, 1);
+    printf("Path on shut down : %s\n", getenv("PATH"));
+    printf("Goodbye.\n");
 
     return 0;
   }
@@ -84,30 +83,66 @@ int main() {
 //   return tokenisedArray;
 // }
 
-// void exCommand(char cmd[30], char *tokenisedArray[MAX_ARGS]) {
-//
-// 	pid_t pid = fork();
-//
-//   printf("%s\n", cmd);
-// 	//fork returns a negative value: error has occured
-// 	if (pid == -1) {
-// 		perror("Error\n");
-//     exit(1);
-// 	}
-//
-// 	//fork returns 0: the child process is running
-// 	else if (pid == 0) {
-//          if(execvp(tokenisedArray[0],tokenisedArray) == -1 ){
-//                printf("%s: Command not found\n",tokenisedArray[0]);
-//
-//          }
-//
-// 	}
-// 	//parent process
-// 	else {
-//     int status;
-// 		waitpid(pid, &status, 0);
-// 		printf("Child Complete\n");
-// 	}
-//
-// }
+void get_path(char *tokenisedArray[MAX_ARGS]){
+  if (tokenisedArray[1]!=NULL){
+    printf("getpath function does not have parameters\n");
+  }
+  else{
+      printf("Current path is : %s\n", getenv("PATH"));
+  }
+
+}
+
+void set_path(char *tokenisedArray[MAX_ARGS]){
+
+  if (tokenisedArray[1]!=NULL){
+    if (tokenisedArray[2]!=NULL){
+      printf("Too many parameters!\n");
+    }
+    else{
+      if (setenv("PATH", tokenisedArray[1], 1)==0){
+        printf("Path set successfully.\n");
+      }
+      else{
+        printf("Path doesn't exist.\n");
+      }
+    }
+  }
+  else{
+    printf("Path needs to be included.\n");
+  }
+}
+
+void exCommand(char *tokenisedArray[MAX_ARGS]) {
+
+  if (strcmp("getpath", tokenisedArray[0])==0){
+    get_path(tokenisedArray);
+  }
+  else if (strcmp("setpath", tokenisedArray[0])==0){
+    set_path(tokenisedArray);
+  }
+  else{
+    pid_t pid = fork();
+  	//fork returns a negative value: error has occured
+  	if (pid == -1) {
+  		perror("Error\n");
+      exit(1);
+  	}
+  	//fork returns 0: the child process is running
+  	else if (pid == 0) {
+           if(execvp(tokenisedArray[0],tokenisedArray) == -1 ){
+                 printf("%s: Command not found\n",tokenisedArray[0]);
+
+           }
+
+  	}
+  	//parent process
+  	else {
+      int status;
+  		waitpid(pid, &status, 0);
+  		printf("Child Complete\n");
+  	}
+  }
+
+
+}
